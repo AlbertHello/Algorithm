10ad33f216bbdb8cc7d56aa7b57f7d756d85420f
隔壁老王 <815878037@qq.com>
2019-01-11 13:02:54 +0800

    算法优化

--- LeetCode1-10/LeetCode1-10/LeetCode1-10/ViewController.swift
+++ LeetCode1-10/LeetCode1-10/LeetCode1-10/ViewController.swift
@@ -2,8 +2,8 @@
 //  ViewController.swift
 //  LeetCode1-10
 //
-//  Created by WenDu3783 on 2018/2/2.
-//  Copyright © 2018年 文都. All rights reserved.
+//  Created by Albert on 2018/2/2.
+//  Copyright © 2018年 58. All rights reserved.
 //
 
 import UIKit
@@ -13,42 +13,41 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor=UIColor.brown
+        let testArray = [2,7,11,15];
+        let target = 26;
+        let indexArray = twoSum(testArray, target);
+        print(indexArray);
+        
+        
     }
 }
 
 
-// 业务
 extension ViewController {
     
-    //1 TwoSum
+    //1. TwoSum
     fileprivate func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-        var num1=0
-        var num2=0
         var i=0
         var j=0
-        var flag = false
         while i<nums.count-1 {
-            num1=nums[i]
             j = i+1
             while j<nums.count{
-                num2=nums[j]
-                if num2+num1 == target {
-                    flag=true
-                    break
+                if nums[i]+nums[j] == target {
+                    return [i,j]
                 }
                 j+=1
             }
-            if flag {
-                break
-            }
             i+=1
         }
-        return [i, j]
+        return []
     }
+    /**
+     复杂度分析：
+     时间复杂度：O(n2)。对于每个元素，我们试图通过遍历数组的其余部分来寻找它所对应的目标元素，这将耗费
+O(n)的时间。因此时间复杂度为O(n2)。
+     空间复杂度：O(1)。
+     */
     
-    
-    
-    
-    
-    
+
 }
+
